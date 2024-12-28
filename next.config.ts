9792df3d05bf75d7917bbefea19cce61399ec5ac
visorry <vishnuvisa00@gmail.com>
2024-12-29 04:31:29 +0530

    feat: update image domains and add new dependencies

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com", "via.placeholder.com", "picsum.photos", "source.unsplash.com"], // Add the domains you need here
+    domains: ["images.squarespace-cdn.com", "i.etsystatic.com", "plus.unsplash.com", "via.placeholder.com", "images.unsplash.com", "source.unsplash.com"], // Add the domains you need here
   },
 };
 
